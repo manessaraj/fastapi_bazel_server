71b2117c2040aea7477ecd4dc9be17e08d2fbbf8
Saraj Singh Manes <manes.saraj@gmail.com>
2023-08-18 23:24:35 -0400

    FA-0 Initial setup of app, docker container works

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from typing import Optional
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8080, host="0.0.0.0", log_level="info")
